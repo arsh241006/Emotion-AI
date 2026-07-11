d29357769a407c6739f798dd34adbd6dac862597
arsh241006 <arshpreet.1661@gmail.com>
2026-07-11 16:58:36 +0530

    Refactor app configuration and update requirements for improved dependency management

--- backend/utils/model_loader.py
+++ backend/utils/model_loader.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from tensorflow.keras.models import load_model
 
-MODEL_PATH = "../saved_models/emotion_model_v2.keras"
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_PATH = BASE_DIR.parent / "saved_models" / "emotion_model_v2.keras"
 
 model = load_model(MODEL_PATH)
